2cf03cac742fdc038ac619849848de675b0aeae0
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-04-27 18:48:29 -0400

    se modifica el listado de los usuarios

--- tienda/src/main/java/com/tiendaonline/tienda/Controller/UsuarioController.java
+++ tienda/src/main/java/com/tiendaonline/tienda/Controller/UsuarioController.java
@@ -1,28 +1,36 @@
 package com.tiendaonline.tienda.Controller;
 
+
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import com.tiendaonline.tienda.Service.UsuarioService;
-
-import com.tiendaonline.tienda.Model.Usuario;
-
-
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.validation.Valid;
+
+import com.tiendaonline.tienda.Service.UsuarioService;
+import com.tiendaonline.tienda.Model.Usuario;
 
 
 
@@ -40,6 +48,7 @@ public class UsuarioController
     private UsuarioService usuarioService;
 
 
+    /*
     //mostrar listado de todos los usuarios
     @GetMapping
     public List<Usuario>getAllUsuarios()
@@ -47,6 +56,28 @@ public class UsuarioController
         log.info("Retornando Todos los usuarios");  
         return usuarioService.getAllUsuarios();
     }
+    */
+
+        @GetMapping
+     public CollectionModel<EntityModel<Usuario>> getAllUsuarios()
+     {
+        log.info("GET /usuarios");
+        log.info("Retornando todos los usuarios");
+        List<Usuario> usuarios = usuarioService.getAllUsuarios();
+
+        List<EntityModel<Usuario>> usuarioResources = usuarios.stream()
+            .map(usuario -> EntityModel.of(usuario,
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getUsuarioById(usuario.getUserId())).withSelfRel()
+                    ))
+            .collect(Collectors.toList());
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllUsuarios());
+        CollectionModel<EntityModel<Usuario>> resources = CollectionModel.of(usuarioResources, linkTo.withRel("usuarios")); 
+
+        return resources;
+     }
+
+
 
 
 
