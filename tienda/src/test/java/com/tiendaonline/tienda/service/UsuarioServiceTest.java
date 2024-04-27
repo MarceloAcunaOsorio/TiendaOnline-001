e0f772b4194c8a20644bd29594081aa023fdfa4a
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-04-27 09:37:49 -0400

    se configura Service Test

---
+++ tienda/src/test/java/com/tiendaonline/tienda/service/UsuarioServiceTest.java
@@ -0,0 +1,51 @@
+package com.tiendaonline.tienda.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.tiendaonline.tienda.Model.Usuario;
+import com.tiendaonline.tienda.Repository.UsuarioRepository;
+import com.tiendaonline.tienda.Service.UsuarioServiceImpl;
+
+
+
+@ExtendWith(MockitoExtension.class)
+public class UsuarioServiceTest 
+{
+   @InjectMocks
+   private UsuarioServiceImpl usuarioServicio;
+
+
+
+
+   @Mock
+   public UsuarioRepository usuarioRepositorioMock;
+
+
+
+
+   @Test
+   public void guardarUsuarioTest()
+   {
+     //Arrange
+     Usuario usuario = new Usuario();
+     usuario.setNombreuser("Jose Randon");
+
+     when(usuarioRepositorioMock.save(any())).thenReturn(usuario);
+
+     //Act
+     Usuario resultado = usuarioServicio.createUsuario(usuario);
+
+
+     //Assert
+     assertEquals("Jose Randon", resultado.getNombre_User());
+
+   }
+}
